f75735db00c36aae1e663db7c96f24b0df727f2d
janramirez <jramirez.ust@gmail.com>
2023-08-21 23:45:31 +0800

    Created onboarding form using shadcn-ui

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -9,7 +9,10 @@ function Topbar() {
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
         <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">Marites</p>
+        <div className="flex flex-col">
+          <p className="text-heading3-bold text-light-1 max-xs:hidden">Marites</p>
+          <p className="text-light-1">Mare 'eto latest!</p>
+        </div>
       </Link>
 
       <div className="flex items-center gap-1">
