0a76671ed1670b7b449aa2caf76648109f6f04b5
janramirez <jramirez.ust@gmail.com>
2023-08-22 16:26:13 +0800

    Created backend using mongoose, connected to MongoDB database

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -12,6 +12,8 @@ import { ChangeEvent, useState } from 'react';
 import { Button } from '../ui/button';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
     user: {
@@ -29,6 +31,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
     const [files, setFiles] = useState<File[]>([])
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -75,7 +79,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             }
         }
         
-        // TODO: Update User profile
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        });
+
+        if(pathname === '/profile/edit') {
+            router.back();
+        } else {
+            router.push('/');
+        }
     }
 
     return (
