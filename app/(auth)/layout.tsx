4003bd78b0a3a02d960c19c256c0c38b05c8dec0
janramirez <jramirez.ust@gmail.com>
2023-08-23 22:14:01 +0800

    fix: aligned Clerk sign-in card

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,22 +1,30 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
-import '../globals.css';
+import "../globals.css";
 
-export const metadata ={
-    title: 'Marites',
-    description: 'A Next.js 13 Marites - A Threads clone application for the marites'
-}
+export const metadata = {
+    title: "Marites",
+    description:
+        "A Next.js 13 Marites - A Threads clone application for the marites",
+};
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: {children: React.ReactNode}) 
-{
-    return <ClerkProvider>
-        <html lang="en">
-            <body className={`${inter.className} bg-dark-1`}>
-                {children}
-            </body>
-        </html>
-    </ClerkProvider>
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    <div className="w-full flex justify-center items-center min-h-screen">
+                        {children}
+                    </div>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
